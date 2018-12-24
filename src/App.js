d5acd4c12b57ecd02eb6644fbfd94c6b5be95b15
Carlos <mezoico@msn.com>
2018-12-24 15:54:47 -0600

    Bring files over from original matcher program

--- src/App.js
+++ src/App.js
@@ -1,28 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import _ from 'lodash';
+import { data } from './data.js';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const diseaseNames = _.shuffle(data.map((item, index) => ({diseaseName: item.diseaseName, index: index}) ));
+const pathogens = _.shuffle(data.map((item, index) => ({pathogen: item.pathogen, index: index})));
+
+const App = props => (
+  <div className='container'>
+  <br />
+  <div className='row'>
+    <div className='col-md-6'>
+      Description
+      <hr />
+
+    </div>
+    <div className='col-md-6'>
+      Answer
+      <hr />
+
+    </div>
+  </div>
+  
+  
+  <div className='row'>
+    <div className='col-md-6'>
+      col1
+    </div>
+    <div className='col-md-6'>
+      col2
+    </div>
+  </div>
+
+  <div className='row'>
+    <div className='col-md-6'>
+      col1
+    </div>
+    <div className='col-md-6'>
+      col2
+    </div>
+  </div>
+
+  <div className='row'>
+    <div className='col-md-6'>
+      col1
+    </div>
+    <div className='col-md-6'>
+      col2
+    </div>
+  </div>
+
+  <div className='row'>
+    <div className='col-md-6'>
+      col1
+    </div>
+    <div className='col-md-6'>
+      col2
+    </div>
+  </div>
+</div>
+);
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const data = [
+    {
+        pathogen: 'Cytomegalovirus (CMV)',
+        diseaseName: 'Inclusion disease',
+        mot: 'Contact with body secretions such as saliva, urine, seman, vaginal secretions, and breast milk',
+        ss: 'Fetal birth defects; in adults symptoms sometimes include prolonged fever and mild hepatitis'
+    },
+    {
+        pathogen: 'Ebola Virus',
+        diseaseName: 'Ebola hemorrhagic fever',
+        mot: 'Body fluid contact. Causes internal bleeding.',
+        ss: 'Acute fever, headache, arthritis, muscle pain, sore throat, weakness, diarrhea, vomitting, stomach pain, rash, red eyes, hiccups, internal and external bleeding'
+    },
+    {
+        pathogen: 'Encephalitis virus',
+        diseaseName: 'Encephalitis',
+        mot: 'Mosquito bite',
+        ss: 'Chills, fever, headache, confusion, coma'
+    },
+    {
+        pathogen: 'Epstein-Barr virus',
+        diseaseName: 'Mononucleosis (kissing disease; college disease)',
+        mot: 'Transfer of saliva',
+        ss: 'Infection of the paratoid salivary glands, fever, sore throat, fatigue'
+    }
+];
